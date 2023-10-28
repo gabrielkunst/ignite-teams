4f2de392e5e5ae65044b4ce56dd9b54df315e3ab
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-28 12:27:21 -0300

    feat - added header component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
@@ -7,13 +6,19 @@ import {
 	Roboto_700Bold,
 	useFonts,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
 	return (
 		<ThemeProvider theme={theme}>
-			{fontsLoaded ? <Groups /> : <ActivityIndicator />}
+			{fontsLoaded ? <Groups /> : <Loading />}
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor={theme.COLORS.GRAY_700}
+			/>
 		</ThemeProvider>
 	);
 }

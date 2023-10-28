530aca5b1dc6cb13b63a49bce41dea8f359d9e9a
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-28 11:50:28 -0300

    feat - adding google fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
+import {
+	Roboto_400Regular,
+	Roboto_700Bold,
+	useFonts,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
 	return (
 		<ThemeProvider theme={theme}>
-			<Groups />
+			{fontsLoaded ? <Groups /> : <ActivityIndicator />}
 		</ThemeProvider>
 	);
 }

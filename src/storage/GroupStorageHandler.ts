def86be289be5ac232c431cc7896f95eadf33ea5
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-12-11 22:47:07 -0300

    feat: added group storage functions

---
+++ src/storage/GroupStorageHandler.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GROUP_COLLECTION } from "./storageConfig";
+
+export class GroupStorageHandler {
+	static async getAllGroups() {
+		try {
+			const groupsJson = await AsyncStorage.getItem(GROUP_COLLECTION);
+
+			if (!groupsJson) {
+				return [];
+			}
+
+			const groups = JSON.parse(groupsJson);
+			return groups;
+		} catch (error) {
+			console.error(error);
+			return [];
+		}
+	}
+
+	static async createGroup(newGroup: string) {
+		try {
+			const allGroups = await this.getAllGroups();
+			const newGroupList = [...allGroups, newGroup];
+
+			const newGroupListJson = JSON.stringify(newGroupList);
+			await AsyncStorage.setItem(GROUP_COLLECTION, newGroupListJson);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}
